5ad8f3b74d899df3a408191927fc02675541d84e
leonardit-1 <127370125+leonardit-1@users.noreply.github.com>
2023-09-09 18:56:49 +1000

    commit with mern stack template

--- react/timetable/src/App.js
+++ react/timetable/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [helloWorld, setHelloWorld] = useState(0);
+  useEffect(() => {
+    async function getHelloWorld() {
+      const response = await fetch("http://localhost:3001/helloworld/");   
+      if (!response.ok) {
+        window.alert(`Error: ${response.statusText}`);
+        return;
+      }
+      setHelloWorld(await response.text());
+    }
+    getHelloWorld();
+    return;
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>A Test React Application</h1>
+        <p>Message from back-end server: {helloWorld}</p>
       </header>
     </div>
   );
