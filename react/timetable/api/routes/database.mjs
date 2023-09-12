ac334ec150ed3cf8d2308f695d9c946a4530a448
Alex <alexhughes@iinet.net.au>
2023-09-12 22:48:01 +1000

    Testing connection to database

--- react/timetable/api/routes/database.mjs
+++ react/timetable/api/routes/database.mjs
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@test.mzctw6w.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ashughes:randomstring@test.mzctw6w.mongodb.net/?retryWrites=true&w=majority";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -8,16 +9,27 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        await client.connect();
+    
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
+
 run().catch(console.dir);
