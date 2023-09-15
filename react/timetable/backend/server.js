7aa4de1f645ef600ac3d443247e045c934bca41a
Yong Jun Loo <115601493+ahzunzun@users.noreply.github.com>
2023-09-15 16:18:55 +1000

    replaced initial mjs file with js file and connected DB

---
+++ react/timetable/backend/server.js
@@ -0,0 +1,24 @@
+// Load env variables
+if (process.env.NODE_ENV != 'production'){
+    require("dotenv").config();
+}
+
+
+
+// Import dependencies 
+const express = require('express')
+const connectToDb = require("./config/connectToDb");
+
+// Create an express app
+const app = express()
+
+// Connect to database
+connectToDb();
+
+// Routing
+app.get('/', (req, res) => {
+    res.json({hello: "world"});
+})
+
+// Start our server
+app.listen(process.env.PORT);
