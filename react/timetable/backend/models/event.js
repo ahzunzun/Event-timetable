11e2eb0ec5828755ba399ff674e7bdbe2cae3116
Yong Jun Loo <115601493+ahzunzun@users.noreply.github.com>
2023-09-20 16:10:08 +1000

    added more descriptive comments to event.js file

--- react/timetable/backend/models/event.js
+++ react/timetable/backend/models/event.js
@@ -2,28 +2,32 @@ const moment = require("moment");
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
-    title: {type: String, required: [true, "Please write a title for your event"]},
-    start: {
+  // Store the event title attribute in the schema  
+  title: {type: String, required: [true, "Please write a title for your event"]},
+  // Store event's start date attribute in the schema
+  start: {
      type: Date,
      required: [true, "Please Insert The Start of your event" ],
      min: [new Date(), "can't be before now!!"],
-    },
-    end: {
-     type: Date,
-     // Setting a min function to accept any date one hour ahead of start
-     min: [function(){
-       const date = new Date(this.start)
-       const validDate = new Date(date.setHours(date.getHours()+1)) 
-       return validDate
-     },"Event End must be at least one hour a head of event time"],
+  },
+  // Store event's end date attribute in schema
+  end: {
+    type: Date,
+    // Setting a min function to accept any date one hour ahead of start
+    min: [function(){
+      const date = new Date(this.start)
+      const validDate = new Date(date.setHours(date.getHours()+1)) 
+      return validDate
+    },"Event End must be at least one hour a head of event time"],
     default: function(){
       const date = new Date(this.start)
       return date.setDate(date.getDate()+1)
     },
-    },
-    // Venue attribute
-    venue: {type: String, required: [true, "Please choose a venue for your event"]},
-    describe: { type: String},
+  },
+  // Venue attribute
+  venue: {type: String, required: [true, "Please choose a venue for your event"]},
+  // Event's description attribute
+  describe: { type: String},
 });
 
 // Check if venue clashes with time
