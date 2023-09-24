8597e5c4e3a5b136c91de2f1ecc9d1044b933fc0
YarrakBoi <kcaminatra@gmail.com>
2023-09-24 13:54:51 +1000

    Changed css and js of eventPopup

--- react/timetable/src/components/EventPopup.js
+++ react/timetable/src/components/EventPopup.js
@@ -1,38 +1,37 @@
 import React, { useState } from 'react';
-import './EventPopup.css'; // Create this CSS file for styling
+import './EventPopup.css';
 import Popup from 'reactjs-popup';
 
 
 function EventPopup() {
-
   return (
     <Popup trigger={<button className='popup-button'>Add Event</button>}>
       {close => (
         <div className='popup'>
         <div className='popup-content'>
-        <h2>Event Details</h2>
+        <h2 className="form-title">Event Details</h2>
             <form>
               <div className="form-group">
-                <label htmlFor="title">Title</label>
+                <label htmlFor="title">Title </label>
                 <input type="text" id="title" />
               </div>
               <div className="form-group">
-                <label htmlFor="date">Date</label>
+                <label htmlFor="date">Date </label>
                 <input type="date" id="date" />
               </div>
               <div className="form-group">
-                <label htmlFor="time">Time</label>
+                <label htmlFor="time">Time </label>
                 <input type="time" id="time" />
               </div>
               <div className="form-group">
-                <label htmlFor="place">Place</label>
+                <label htmlFor="place">Place </label>
                 <input type="text" id="place" />
               </div>
               <div className="form-group">
-                <label htmlFor="description">Event Description</label>
+                <label htmlFor="description">Event Description </label>
                 <textarea id="description" rows="4"></textarea>
               </div>
-              <button type="submit">Create Event</button>
+              <button type="submit">Create Event </button>
             </form>
             <button className="close-button" onClick={close}>
               Close
