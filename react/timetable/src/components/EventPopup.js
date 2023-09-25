5f77f0904f13972665ef24c8905a99bfd10c7ee3
leonardit-1 <127370125+leonardit-1@users.noreply.github.com>
2023-09-25 21:02:58 +1000

    addevent popup sends the event data to the Database

--- react/timetable/src/components/EventPopup.js
+++ react/timetable/src/components/EventPopup.js
@@ -1,38 +1,67 @@
 import React, { useState } from 'react';
 import './EventPopup.css';
 import Popup from 'reactjs-popup';
+import axios from 'axios';
 
 
 
 function EventPopup() {
+// state
+const [createForm, setCreateForm] = useState({
+  title: '',
+  start: new Date(),
+  end: new Date(),
+  describe: '',
+  venue: ''
+})
+// functions
+const updateCreateFormField = (e) => {
+  const {name, value} = e.target;
+  setCreateForm({
+    ...createForm,
+    [name]: value,
+  })
+  console.log(createForm);
+}
+const createEvent = async (e) => {
+  e.preventDefault();
+  console.log(createForm);
+  const res = await axios.post("http://localhost:3000/api/events", createForm)
+  
+}
+
+
+
+
+
   return (
     <Popup trigger={<button className='popup-button'>Add Event</button>}>
       {close => (
         <div className='popup'>
         <div className='popup-content'>
         <h2 className="form-title">Event Details</h2>
-            <form>
+            <form onSubmit = {createEvent}>
               <div className="form-group">
                 <label htmlFor="title">Title </label>
-                <input type="text" id="title" />
+                <input onChange = {updateCreateFormField} type="text" name="title" />
               </div>
               <div className="form-group">
-                <label htmlFor="date">Date </label>
-                <input type="date" id="date" />
+                <label htmlFor="datetime-local">Start Date </label>
+                <input onChange = {updateCreateFormField} type="datetime-local" name="start" />
               </div>
               <div className="form-group">
-                <label htmlFor="time">Time </label>
-                <input type="time" id="time" />
+                <label htmlFor="datetime-local">End Date </label>
+                <input onChange = {updateCreateFormField} type="datetime-local" name="end" />
               </div>
               <div className="form-group">
                 <label htmlFor="place">Place </label>
-                <input type="text" id="place" />
+                <input onChange = {updateCreateFormField} type="text" name="venue" />
               </div>
               <div className="form-group">
                 <label htmlFor="description">Event Description </label>
-                <textarea id="description" rows="4"></textarea>
+                <textarea onChange = {updateCreateFormField} name="describe" rows="4"></textarea>
               </div>
-              <button type="submit">Create Event </button>
+              <button type="submit" >Create Event </button>
             </form>
             <button className="close-button" onClick={close}>
               Close
