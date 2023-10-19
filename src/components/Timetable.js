e392b1e4d61d73636ef370027768273353e61e29
leonardit-1 <127370125+leonardit-1@users.noreply.github.com>
2023-10-19 22:11:35 +1100

    test

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -74,7 +74,7 @@ const MyCalendar = ({myEventsList, showEventApi, showEventsApi}) => {
         renderStatus = {renderStatus} 
         reRender = {reRender}
       />
-      {open && <ShowEventPopup
+      {open && <ShowEventU
         open = {open}
         event={showEvent}
         handleOpen = {openEventClick}
