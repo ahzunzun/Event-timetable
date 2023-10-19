b5a1c018dc8eab5b892e32baac5958ffe812b50a
YarrakBoi <kcaminatra@gmail.com>
2023-10-19 21:20:22 +1100

    removal of some line leads to app errors, readded Router in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {BrowserRouter as  Routes,  Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes,  Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Logo from "./components/images/logo-1.webp"
 import MyCalendar from './components/Timetable';
